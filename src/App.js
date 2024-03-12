01ff8ac9d0b33b323e12f5ce5dbfccdaecd320af
Priyank-kapadia <163159732+Priyank-kapadia@users.noreply.github.com>
2024-03-12 21:15:55 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,39 @@
+import React, { useState } from 'react';
+import Navigation from './components/Navigation';
+import Text from './components/Text';
+import Alerts from './components/Alerts';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes, // replace Switch with Routes
+  Route,
+} from "react-router-dom";
 
+function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (type, message) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2500);
+  }
+  return (
+    <>
+      <Router>
+        <Navigation />
+        <Alerts alert={alert} />
+        <div className="container my-2">
+          <Routes>
+            <Route excact path="/about" element={<About />} />
+            <Route exact path="/" element={<Text showAlert={showAlert} heading="UpperCase Converter" />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
